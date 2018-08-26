1de33ec2071947d299965e2cec7b65b54bd53fa8
Anthony Lapham <lapham.2@wright.edu>
2018-08-26 10:53:54 -0400

    styling

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index');
